fec1dc72ccf73afb46357efcae24dc76d5333559
Xu Xue <juniorxxue@gmail.com>
2021-07-13 18:55:21 +0800

    gui beautified

--- init.el
+++ init.el
@@ -12,9 +12,31 @@
   (load bootstrap-file nil 'nomessage))
 
 ;; vertical minibuffer
-(straight-use-package 'selectrum)
-(require 'selectrum)
-(selectrum-mode +1)
+(straight-use-package 'ivy)
+(straight-use-package 'counsel)
+(ivy-mode)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+;; enable this if you want `swiper' to use it
+;; (setq search-default-mode #'char-fold-to-regexp)
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> o") 'counsel-describe-symbol)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+
 
 ;; better-defaults
 (straight-use-package
@@ -35,13 +57,31 @@
   (exec-path-from-shell-initialize))
 
 ;; theme
+;;(straight-use-package
+;;'(github-theme :type git :host github :repo "juniorxxue/github-theme"))
+;;(load-theme 'github t)
+(set-frame-font "Source Code Pro 12")
+(setq-default cursor-type 'bar)
+
+(straight-use-package 'spacemacs-theme)
+(require 'spacemacs-light-theme)
+(load-theme 'spacemacs-light t)
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar .t))
+
 (straight-use-package
- '(github-theme :type git :host github :repo "juniorxxue/github-theme"))
-(load-theme 'github t)
+  '(splash-screen :type git :host github :repo "rougier/emacs-splash"))
+(require 'splash-screen)
 
 (straight-use-package 'mood-line)
 (require 'mood-line)
 (mood-line-mode)
+
+;; scroll
+(straight-use-package 'good-scroll)
+(require 'good-scroll)
+(good-scroll-mode 1)
+(global-set-key (kbd "M-j") #'good-scroll-up)
+(global-set-key (kbd "M-k") #'good-scroll-down)
 
 ;; proof-general
 (straight-use-package 'proof-general)
